b853237fb98b4f8587962e14369a5167ec4c099f
wangjiangit <jiji_12342006@163.com>
2019-07-09 07:59:46 +0800

    非入侵式接口

--- demo.go
+++ demo.go
@@ -57,7 +57,7 @@ func (base *Base) Foo() {}
 func (base *Base) Bar() {}
 
 type Foo struct {
-	 Base
+	Base
 
 	// *Base
 }
@@ -66,6 +66,62 @@ func (foo *Foo) Bar() {
 	foo.Base.Bar()
 
 }
+
+// 侵入式接口(实现类需要明确声明自己实现了某个接口)
+
+// 非入侵式接口
+//一个类只需要实现了接口要求的所有函数，我们就说这个类实现了该接口。
+type File struct {
+}
+
+func (file *File) Read(buf []byte) (n int, err error) {
+	return 1, errors.New("error1")
+}
+func (f *File) Write(buf []byte) (n int, err error) {
+	return 1, errors.New("error2")
+}
+
+func (f *File) Seek(off int64, whence int) (pos int64, err error) {
+	return 1, errors.New("error3")
+}
+
+func (f *File) Close() error {
+	return nil
+}
+
+type IFile interface {
+	Read(buf []byte) (n int, err error)
+	Write(buf []byte) (n int, err error)
+	Seek(off int64, whence int) (pos int64, err error)
+	Close() error
+}
+
+type IReader interface {
+	Read(buf []byte) (n int, err error)
+}
+type IWriter interface {
+	Write(buf []byte) (n int, err error)
+}
+type ICloser interface {
+	Close() error
+}
+
+
+var file1 IFile=new(File)
+var file2 IReader = new(File)
+var file3 IWriter = new(File)
+var file4 ICloser = new(File)
+/*
+Go语言的非侵入式接口，看似只是做了很小的文法调整，实则影响深远。
+其一，Go语言的标准库，再也不需要绘制类库的继承树图。你一定见过不少C++、Java、C#
+类库的继承树图。这里给个Java继承树图：
+http://docs.oracle.com/javase/1.4.2/docs/api/overview-tree.html
+在Go中，类的继承树并无意义，你只需要知道这个类实现了哪些方法，每个方法是啥含义
+就足够了。
+其二，实现类的时候，只需要关心自己应该提供哪些方法，不用再纠结接口需要拆得多细才
+合理。接口由使用方按需定义，而不用事前规划。
+其三，不用为了实现一个接口而导入一个包，因为多引用一个外部的包，就意味着更多的耦
+合。接口由使用方按自身需求来定义，使用方无需关心是否有其他模块定义过类似的接口。*/
 
 func main() {
 
