3503f4d8d491132fe93e8fb264af5eaad5c13942
wangjiangit <jiji_12342006@163.com>
2019-07-14 21:17:35 +0800

    并发编程

--- concurrent.go
+++ concurrent.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+func goAdd(x, y int, ch chan int) {
+	ch <- 1
+	fmt.Println("hello world1")
+}
 func main() {
-	
+	var ch chan int
+	ch=make(chan int,1)
+	go goAdd(1, 2, ch)
+	<-ch
+
 }
