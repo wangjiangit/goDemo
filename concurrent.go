803fb1c91c6e9634054a92534a60efd626ba9204
wangjiangit <jiji_12342006@163.com>
2019-08-02 19:25:59 +0800

    channel的传递

--- concurrent.go
+++ concurrent.go
@@ -192,4 +192,21 @@ func main() {
 
 	}*/
 
+	// channel的传递
+	/*
+		type PipeData struct {
+			value int
+			handler func(int) int
+			next chan int
+		}
+		func handle(queue chan *PipeData){
+			for data:=range queue {
+				data.next <- data.handler(data.value)
+			}
+		}
+	*/
+
+
+	
+
 }
